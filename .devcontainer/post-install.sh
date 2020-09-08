cfebf457e4de4a39f9ad847ff15a0939cb2c2bc5
Russ Mckendrick <russ@mckendrick.io>
2020-09-08 11:29:08 +0000

    Update

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -16,11 +16,28 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends apt-utils dialog
 
 # update / install utils
-sudo apt-get install -y --no-install-recommends dnsutils httpie wget unzip curl bash-completion
+sudo apt-get install -y --no-install-recommends dnsutils httpie wget unzip curl bash-completion jq
 DEBIAN_FRONTEND=dialog
 
 # install terraform
+
+getLatest() {
+      LATEST_ARR=($(wget -q -O- https://api.github.com/repos/hashicorp/terraform/releases 2> /dev/null | awk '/tag_name/ {print $2}' | cut -d '"' -f 2 | cut -d 'v' -f 2))
+      ;;
+  esac
+
+for ver in "${LATEST_ARR[@]}"; do
+  if [[ ! $ver =~ beta ]] && [[ ! $ver =~ rc ]] && [[ ! $ver =~ alpha ]]; then
+    LATEST="$ver"
+    break
+  fi
+done
+echo -n "$LATEST"
+}
+
+VERSION=$(getLatest)
+
 cd ~
-wget https://releases.hashicorp.com/terraform/0.13.0/terraform_0.13.0_linux_amd64.zip
-unzip terraform_0.13.0_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/$VERSION/terraform_$VERSION_linux_amd64.zip
+unzip terraform_$VERSION_linux_amd64.zip
 sudo install terraform /usr/local/bin/
